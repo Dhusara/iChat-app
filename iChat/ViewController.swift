5b6ae02773d7a0640e83b158598428f306b7a6dd
Sergei Demianenko <siriysgrey@gmail.com>
2020-09-02 18:03:30 +0300

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Serhii Demianenko on 02.09.2020.
+//  Copyright © 2020 Serhii Demianenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
