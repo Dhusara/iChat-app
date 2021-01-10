6e5b4c8edc5ce6ed0ff5888f21dd5a0ff9c440dd
Sergei <dhusara@icloud.com>
2021-01-10 12:06:07 +0200

    adding UICollectionView

--- iChat/Controllers/MainTabBarController.swift
+++ iChat/Controllers/MainTabBarController.swift
@@ -16,9 +16,21 @@ class MainTabBarController: UITabBarController {
         let listViewController = ListViewController()
         let peopleViewController = PeopleViewController()
         
-        viewControllers = [listViewController, peopleViewController]
+        tabBar.tintColor = #colorLiteral(red: 0.6274509804, green: 0.2156862745, blue: 0.968627451, alpha: 1)
+        let boldConfiguration = UIImage.SymbolConfiguration(weight: .medium)
+        let peopleImage = UIImage(systemName: "person.2", withConfiguration: boldConfiguration)!
+        let conversationImage = UIImage(systemName: "bubble.left.and.bubble.right", withConfiguration: boldConfiguration)!
+        
+        viewControllers = [generateNavigationController(rootViewController: peopleViewController, title: "People", image: peopleImage), generateNavigationController(rootViewController: listViewController, title: "Conversations", image: conversationImage)]
     }
     
+    private func generateNavigationController(rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.title = title
+        navigationVC.tabBarItem.image = image
+        
+        return navigationVC
+    }
     
 }
 
