10eb52274c9422aa90537270159189727d6a728a
Sergei <dhusara@icloud.com>
2021-01-10 11:46:03 +0200

    adding mvc

---
+++ iChat/Controllers/MainTabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  MainTabBarController.swift
+//  iChat
+//
+//  Created by Serhii Demianenko on 10.01.2021.
+//  Copyright © 2021 Serhii Demianenko. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let listViewController = ListViewController()
+        let peopleViewController = PeopleViewController()
+        
+        viewControllers = [listViewController, peopleViewController]
+    }
+    
+}
+
---
+++ iChat/Controllers/PeopleViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PeopleViewController.swift
+//  iChat
+//
+//  Created by Serhii Demianenko on 10.01.2021.
+//  Copyright © 2021 Serhii Demianenko. All rights reserved.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+    
+}
