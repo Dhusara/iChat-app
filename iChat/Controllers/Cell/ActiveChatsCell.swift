36202622081e559562e1663f802ead227225a55d
Sergei <dhusara@icloud.com>
2021-01-13 21:17:18 +0200

    fixing bugs

--- iChat/Controllers/Cell/ActiveChatsCell.swift
+++ iChat/Controllers/Cell/ActiveChatsCell.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 protocol SelfConfiguringCell {
-    static var reuseId: String {get}
+    static var reuseId: String { get }
     func configure(with value: MChat)
 }
 
 class ActiveChatsCell: UICollectionViewCell, SelfConfiguringCell {
+    static var reuseId: String = "ActiveChatCell"
     
-    static var reuseId: String = "ActiveChatsCell"
     
     let friendImageView = UIImageView()
     let friendName = UILabel(text: "User name", font: .laoSangamMN20())
@@ -25,7 +25,6 @@ class ActiveChatsCell: UICollectionViewCell, SelfConfiguringCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .white
-        
         setupConstraints()
         
         self.layer.cornerRadius = 4
@@ -43,54 +42,55 @@ class ActiveChatsCell: UICollectionViewCell, SelfConfiguringCell {
     }
 }
 
-// MARK: - Setup Constraints
+// MARK: - Setup constraints
 extension ActiveChatsCell {
-    
     private func setupConstraints() {
         friendImageView.translatesAutoresizingMaskIntoConstraints = false
-        gradientView.translatesAutoresizingMaskIntoConstraints = false
         friendName.translatesAutoresizingMaskIntoConstraints = false
         lastMessage.translatesAutoresizingMaskIntoConstraints = false
-    
-        friendImageView.backgroundColor = .red
-    
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        
+        friendImageView.backgroundColor = .orange
+        gradientView.backgroundColor = .black
+        
         addSubview(friendImageView)
         addSubview(gradientView)
         addSubview(friendName)
         addSubview(lastMessage)
-    
+        
         NSLayoutConstraint.activate([
             friendImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             friendImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             friendImageView.heightAnchor.constraint(equalToConstant: 78),
             friendImageView.widthAnchor.constraint(equalToConstant: 78)
         ])
-    
+        
         NSLayoutConstraint.activate([
             friendName.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
             friendName.leadingAnchor.constraint(equalTo: friendImageView.trailingAnchor, constant: 16),
             friendName.trailingAnchor.constraint(equalTo: gradientView.leadingAnchor, constant: 16)
         ])
-    
+        
         NSLayoutConstraint.activate([
             lastMessage.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12),
             lastMessage.leadingAnchor.constraint(equalTo: friendImageView.trailingAnchor, constant: 16),
             lastMessage.trailingAnchor.constraint(equalTo: gradientView.leadingAnchor, constant: 16)
         ])
-    
+        
         NSLayoutConstraint.activate([
             gradientView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             gradientView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             gradientView.heightAnchor.constraint(equalToConstant: 78),
             gradientView.widthAnchor.constraint(equalToConstant: 8)
         ])
+        
     }
 }
 
 // MARK: - SwiftUI
 import SwiftUI
 
-struct ActiveChatsProvider: PreviewProvider {
+struct ActiveChatProvider: PreviewProvider {
     static var previews: some View {
         ContainerView().edgesIgnoringSafeArea(.all)
     }
@@ -99,12 +99,13 @@ struct ActiveChatsProvider: PreviewProvider {
         
         let tabBarVC = MainTabBarController()
         
-        func makeUIViewController(context: UIViewControllerRepresentableContext<ActiveChatsProvider.ContainerView>) -> MainTabBarController {
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ActiveChatProvider.ContainerView>) -> MainTabBarController {
             return tabBarVC
         }
         
-        func updateUIViewController(_ uiViewController: ActiveChatsProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ActiveChatsProvider.ContainerView>) {
+        func updateUIViewController(_ uiViewController: ActiveChatProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ActiveChatProvider.ContainerView>) {
             
         }
     }
 }
+
