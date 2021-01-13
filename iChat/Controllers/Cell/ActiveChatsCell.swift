881560cd950cad22e8cb59d1e49da1c0b4af2c78
Sergei <dhusara@icloud.com>
2021-01-13 21:02:51 +0200

    cleaning code

--- iChat/Controllers/Cell/ActiveChatsCell.swift
+++ iChat/Controllers/Cell/ActiveChatsCell.swift
@@ -24,34 +24,66 @@ class ActiveChatsCell: UICollectionViewCell, SelfConfiguringCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .green
+        backgroundColor = .white
+        
         setupConstraints()
+        
+        self.layer.cornerRadius = 4
+        self.clipsToBounds = true
     }
     
     func configure(with value: MChat) {
-        
+        friendImageView.image = UIImage(named: value.userImageString)
+        friendName.text = value.userName
+        lastMessage.text = value.lastMessage
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Setup Constraints
+extension ActiveChatsCell {
     
     private func setupConstraints() {
         friendImageView.translatesAutoresizingMaskIntoConstraints = false
         gradientView.translatesAutoresizingMaskIntoConstraints = false
         friendName.translatesAutoresizingMaskIntoConstraints = false
         lastMessage.translatesAutoresizingMaskIntoConstraints = false
-        
+    
         friendImageView.backgroundColor = .red
-        
+    
         addSubview(friendImageView)
-        
+        addSubview(gradientView)
+        addSubview(friendName)
+        addSubview(lastMessage)
+    
         NSLayoutConstraint.activate([
             friendImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             friendImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             friendImageView.heightAnchor.constraint(equalToConstant: 78),
             friendImageView.widthAnchor.constraint(equalToConstant: 78)
         ])
-    }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        NSLayoutConstraint.activate([
+            friendName.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            friendName.leadingAnchor.constraint(equalTo: friendImageView.trailingAnchor, constant: 16),
+            friendName.trailingAnchor.constraint(equalTo: gradientView.leadingAnchor, constant: 16)
+        ])
+    
+        NSLayoutConstraint.activate([
+            lastMessage.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12),
+            lastMessage.leadingAnchor.constraint(equalTo: friendImageView.trailingAnchor, constant: 16),
+            lastMessage.trailingAnchor.constraint(equalTo: gradientView.leadingAnchor, constant: 16)
+        ])
+    
+        NSLayoutConstraint.activate([
+            gradientView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            gradientView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            gradientView.heightAnchor.constraint(equalToConstant: 78),
+            gradientView.widthAnchor.constraint(equalToConstant: 8)
+        ])
     }
 }
 
