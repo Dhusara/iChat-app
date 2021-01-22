e1c694c8f45ffef8c46c9d2b72e37e5302210bf0
Sergei <dhusara@icloud.com>
2021-01-22 20:32:46 +0200

    adding new file

---
+++ iChat/Controllers/ChatRequestViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ChatRequestViewController.swift
+//  iChat
+//
+//  Created by Serhii Demianenko on 22.01.2021.
+//  Copyright © 2021 Serhii Demianenko. All rights reserved.
+//
+
+import UIKit
+
+class ChatRequestViewController: UIViewController {
+    
+    let containerView = UIView()
+    let imageView = UIImageView(image: #imageLiteral(resourceName: "human6"), contentMode: .scaleAspectFill)
+    let nameLabel = UILabel(text: "Mia Gonsalez", font: .systemFont(ofSize: 20, weight: .light))
+    let aboutMeLabel = UILabel(text: "You have the opportunity to start a new chat", font: .systemFont(ofSize: 16, weight: .light))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+
+
+// MARK: - SwiftUI
+import SwiftUI
+
+struct ChatRequestVCProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let chatRequestVC = ChatRequestViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ChatRequestVCProvider.ContainerView>) -> ChatRequestViewController {
+            return chatRequestVC
+        }
+        
+        func updateUIViewController(_ uiViewController: ChatRequestVCProvider.ContainerView.UIViewControllerType, context:  UIViewControllerRepresentableContext<ChatRequestVCProvider.ContainerView>) {
+            
+        }
+    }
+}
